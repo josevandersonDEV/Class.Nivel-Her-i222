6c531761375a783cfbca0fdb043f2db7b674dfba
josé-vanderson <vandersonjose993@gmail.com>
2023-10-24 21:31:22 -0300

    PROJETO.DIO

---
+++ Héroi.js
@@ -0,0 +1,4 @@
+console.log "testes"
+console.log ("testees22") 
+console.LOG("TESTES3") 
+console.log("testes4") 
---
+++ PROJETODIO.js
@@ -0,0 +1,31 @@
+let nickname ="Ascendente" 
+let XP = "8500"
+// estrutura de decisao
+
+switch (XP){
+    case 0.1000:
+        console.log("Ferro")
+        
+    case 1001.2000:
+            console.log("Bronze")
+
+     case 2001.5000:
+        console.log("Prata")
+        
+        case 6001.7000:
+            console.log("Ouro")
+
+        case 7001.8000:
+            console.log("Platina")
+            
+         case 8001.9000:
+            console.log("Ascendente")
+
+            case 9001.10000
+            console.log("Imortal")
+
+            case 10000.100000:
+            console.log("Radiante")
+
+}
+console.log("O Heroi de nome") + nickname ("Esta no nivel de") XP 
